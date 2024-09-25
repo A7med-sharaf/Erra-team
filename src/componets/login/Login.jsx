51fcfb09dd943d8b2397421ad1c184f0b44c4d2e
A7med-sharaf <a9986939@gmail.com>
2024-09-25 12:15:49 +0300

    last edit:

---
+++ src/componets/login/Login.jsx
@@ -0,0 +1,34 @@
+import "./login.css"
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope , faKey } from '@fortawesome/free-solid-svg-icons'
+
+export default function Login() {
+
+
+
+  
+    
+  
+  return (
+    <div className="Login col-12 col-sm-6 col-md-3 m-auto   mt-5">
+        <div className="card cardlogin shadow p-3 ">
+            <h1 className="text-center anton-regular">Customer Login</h1>
+            <label>Email</label>
+             <div className="input-email mb-3">
+            <FontAwesomeIcon className="icon-email" icon={faEnvelope} />
+           <input  type="email" className="form-control"   placeholder="inter the Email" />
+           </div>
+           <div className="input-password">
+           <FontAwesomeIcon className="icon-password" icon={faKey} />
+            <label>Password</label>
+           <input type="password" className="form-control mb-5" placeholder="inter your password" />
+           </div>
+           <div className="">
+        <button className="btn btn-primary w-100">Login</button>
+        </div>
+            <span className="text-center">Don't have account?<a href="#" >click here</a></span>
+        </div>
+    </div>
+  )
+}
---
+++ src/componets/signup/Signup.jsx
@@ -0,0 +1,39 @@
+import "./signup.css"
+
+export default function Signup() {
+    
+  return (
+        <div className=" col-12 col-sm-6 col-md-3 m-auto   d-flex justify-content-center align-content-center mt-5">
+        <div className="card shadow p-3">
+            <h1 className="text-center">signup</h1>
+            <div className="input-f mt-3">
+                <label htmlFor="" className="lable">Fistname</label>
+                <input type="text" className="form-control" />
+            </div>
+            <div className="input-l mt-3">
+                <label htmlFor="" className="lable">Lastname</label>
+                <input type="text" className="form-control" />
+            </div>
+            <div className="input-p mt-3">
+                <label htmlFor="" className="lable">Password</label>
+                <input type="text" className="form-control" />
+            </div>
+            <div className="input-c mt-3">
+                <label htmlFor="" className="lable">Confirm Password</label>
+                <input type="text" className="form-control" />
+            </div>
+
+            <div className="btn">
+                <button className="btn btn-primary w-100">
+                Register Now
+                </button>
+            </div>
+            <div className="check mt-3 text-center">
+                <input type="checkbox"  />
+                <span>Accepte all trmes & condition? <a href="#">login now</a></span>
+            </div>
+        </div>
+        </div>
+    
+  )
+}
